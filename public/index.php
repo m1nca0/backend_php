23ed5e89c9c2643a66da8277fa88691117797cce
Mihail VASILEV <noname890256@gmail.com>
2026-03-18 16:13:30 +0800

    almost remade project for twing

--- public/index.php
+++ public/index.php
@@ -1,52 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+require_once '../vendor/autoload.php';
+$loader = new \Twig\Loader\FilesystemLoader('../views');
+$twig = new \Twig\Environment($loader);
+$url = $_SERVER["REQUEST_URI"];
+$title = "";
+$template = "";
+$context = [];
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" rel="stylesheet" />
-</head>
+if ($url == "/") {
+  $title = "Главная";
+  $template = "main.twig";
+} elseif (preg_match("#^/sint#", $url)) {
+  $title = "Синтезаторы";
+  $template = "sint.twig";
+} elseif (preg_match("#^/plug#", $url)) {
+  $title = "Плагины";
+  $template = "plug.twig";
+}
 
-<body>
-  <nav class="navbar navbar-expand-lg bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#"><i class="fa-solid fa-music"></i></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/main">Главная</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/sint">Синтезаторы</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/plug">Плагины</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <div class="container">
-    <?php
-    $url = $_SERVER["REQUEST_URI"];
-
-    if (preg_match("#^/main#",$url)) {
-      require "../views/main.php";
-    } elseif (preg_match("#^/sint#",$url)) {
-      require "../views/sint.php";
-    } elseif (preg_match("#^/plug#",$url)) {
-      require "../views/plug.php";
-    }
-    ?>
-  </div>
-
-
-</body>
-
-</html>
+echo $twig->render($template, [
+    "title" => $title
+]);
