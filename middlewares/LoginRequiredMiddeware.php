040f8e698bc4d04dd37abfa0d0a85fa2d4aff820
Mihail VASILEV <noname890256@gmail.com>
2026-05-07 18:56:18 +0800

    made verify with DB

--- middlewares/LoginRequiredMiddeware.php
+++ middlewares/LoginRequiredMiddeware.php
@@ -4,14 +4,21 @@ class LoginRequiredMiddeware extends BaseMiddleware
 {
   public function apply(BaseController $controller, array $context)
   {
-    $valid_user = "user";
-    $valid_password = "12345";
-
     $user = isset($_SERVER['PHP_AUTH_USER']) ? $_SERVER['PHP_AUTH_USER'] : '';
     $password = isset($_SERVER['PHP_AUTH_PW']) ? $_SERVER['PHP_AUTH_PW'] : '';
 
-    if ($valid_user != $user || $valid_password != $password) {
-      header('WWW-Authenticate: Basic realm="Space objects"');
+    $sql = "SELECT id FROM users WHERE username = :user AND password = :password;";
+
+    $query = $controller->pdo->prepare($sql);
+
+    $query->bindValue("user", $user);
+    $query->bindValue("password", $password);
+    $query->execute();
+
+    $valid_user = $query->fetch();
+
+    if ($valid_user === false) {
+      header('WWW-Authenticate: Basic realm="synthesizers"');
       http_response_code(401);
       exit;
     }
